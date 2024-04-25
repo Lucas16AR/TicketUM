e6d68c0f7d8d52233205a4a77306e6681d584cec
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2024-04-25 18:17:46 -0300

    Swagger Flask server deleted, Flask sv added

---
+++ ticketum/config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+class Config:
+
+    def __init__(self):
+        load_dotenv()
+
+    def load_env_variables(self):
+        self.DB_NAME = os.getenv('DB_NAME')
+        self.DB_USER = os.getenv('DB_USER')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD')
+        self.DB_HOST = os.getenv('DB_HOST')
+        self.DB_PORT = os.getenv('DB_PORT')
+        self.DB_ENGINE = os.getenv('DB_ENGINE')
+        self.DB_PATH = os.getenv('DB_PATH')
---
+++ ticketum/main/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Initialize the configuration
+    config = Config()
+    config.load_env_variables()
+
+    print("PATH DE LA BASE DE DATOS: ", config.DB_PATH+config.DB_NAME)
+
+    # Initialize the database
+    if not os.path.exists(config.DB_PATH+config.DB_NAME):
+        os.mknod(config.DB_PATH+config.DB_NAME)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_ENGINE+config.DB_PATH+config.DB_NAME
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    
+    # from ticketum.main import main as main_blueprint
+    # app.register_blueprint(main_blueprint)
+
+    return app
